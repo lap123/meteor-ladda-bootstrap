43954596c5eee24461f90b01be0f20c2429928f3
jda <jdairay@gmail.com>
2014-08-31 21:18:22 +0100

    Upgraded to meteor 0.9.0

--- package.js
+++ package.js
@@ -1,12 +1,15 @@
 Package.describe({
-    summary: 'Ladda Bootstrap'
+    summary: 'Ladda Bootstrap',
+    version: "0.1.0",
+    name: "lap123:ladda-bootstrap",
+    git: "https://github.com/lap123/meteor-ladda-bootstrap.git"
 });
 
-Package.on_use(function(api) {
-    api.use('bootstrap-3', 'client');
+Package.onUse(function(api) {
+    api.use('mizzao:bootstrap-3', 'client');
 
-    api.add_files('lib/ladda-themeless.css', 'client');
-    api.add_files('lib/spin.js', 'client');
-    api.add_files('lib/ladda.js', 'client');
+    api.addFiles('lib/ladda-themeless.css', 'client');
+    api.addFiles('lib/spin.js', 'client');
+    api.addFiles('lib/ladda.js', 'client');
 });
 
